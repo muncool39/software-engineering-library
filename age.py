1fbed33267c0c354d81d72de3c982a3421c546ad
시원 <105481797+muncool39@users.noreply.github.com>
2023-10-19 15:19:35 +0900

    Update age.py

--- age.py
+++ age.py
@@ -4,7 +4,7 @@ import datetime
 current_date = datetime.date.today()
 
 # 사용자의 생년월일 입력 받기
-birth_year = int(input("태어난 년도를 입력하세요 (예: 1990): ")
+birth_year = int(input("태어난 년도를 입력하세요 (예: 2002): ")
 birth_month = int(input("태어난 월을 입력하세요 (1부터 12까지): ")
 birth_day = int(input("태어난 일을 입력하세요: ")
 
