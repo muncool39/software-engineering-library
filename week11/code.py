8fa2a72cc37dcb7bede0a5c50afd934c09946ff1
muncool39 <muncool39@gmail.com>
2023-11-23 15:01:34 +0900

    refact: code

--- week11/code.py
+++ week11/code.py
@@ -11,8 +11,9 @@ a, b = data[:, 0], data[:, 1]
 fig, axes = plt.subplots(2, 2, figsize=(12, 10))
 
 # 기술 통계: 평균과 중앙값
-axes[0, 0].bar(['평균', '중앙값'], [np.mean(a), np.median(a)], color='blue', alpha=0.7, label='변수 1')
-axes[0, 0].bar(['평균', '중앙값'], [np.mean(b), np.median(b)], color='green', alpha=0.7, label='변수 2')
+for i, var in enumerate([a, b]):
+    axes[0, 0].bar(['평균', '중앙값'], [np.mean(var), np.median(var)], alpha=0.7, label=f'변수 {i+1}')
+
 axes[0, 0].legend()
 axes[0, 0].set_title('기술 통계: 평균과 중앙값')
 
@@ -21,7 +22,7 @@ sns.heatmap(np.corrcoef(data.T), annot=True, ax=axes[0, 1])
 axes[0, 1].set_title('상관 분석')
 
 # 변수의 히스토그램
-axes[1, 0].hist([a, b], bins=15, color=['blue', 'green'], alpha=0.7, label=['변수 1', '변수 2'])
+axes[1, 0].hist([a, b], bins=15, alpha=0.7, label=['변수 1', '변수 2'])
 axes[1, 0].legend()
 axes[1, 0].set_title('변수의 히스토그램')
 
@@ -34,4 +35,3 @@ axes[1, 1].set_title('변수 1 대 변수 2의 산점도')
 # 레이아웃 조정 및 표시
 plt.tight_layout()
 plt.show()
-
