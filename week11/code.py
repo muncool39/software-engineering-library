1fe3755f04124df9e311cf12e4e5d8767d20f8ae
muncool39 <muncool39@gmail.com>
2023-11-23 14:55:12 +0900

    refact: code

--- week11/code.py
+++ week11/code.py
@@ -2,17 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-np.random.seed(0); data = np.random.randn(100, 2)
+# 랜덤 데이터 생성
+np.random.seed(0)
+data = np.random.randn(100, 2)
+a, b = data[:, 0], data[:, 1]
+
+# 서브플롯 생성
 fig, axes = plt.subplots(2, 2, figsize=(12, 10))
-a = data[:, 0]; b = data[:, 1]; axes[0, 0].bar(['Mean', 'Median'], [np.mean(a), np.median(a)], 
-color='blue', alpha=0.7); axes[0, 0].bar(['Mean', 'Median'], [np.mean(b), np.median(b)], 
-color='green', alpha=0.7); axes[0, 0].legend(); axes[0, 0].set_title('Descriptive Statistics: 
-Mean and Median')
-sns.heatmap(np.corrcoef(data.T), annot=True, ax=axes[0, 1]); axes[0, 
-1].set_title('Correlation Analysis'); axes[1, 0].hist(a, bins=15, color='blue', alpha=0.7); 
-axes[1, 0].hist(b, bins=15, color='green', alpha=0.7); axes[1, 0].legend(); axes[1, 
-0].set_title('Histogram of Variables')
-axes[1, 1].scatter(a, b, alpha=0.7); axes[1, 1].set_xlabel('Variable 1'); axes[1, 
-1].set_ylabel('Variable 2'); axes[1, 1].set_title('Scatter Plot of Variable 1 vs Variable 2')
-plt.tight_layout(); plt.show()
+
+# 기술 통계: 평균과 중앙값
+axes[0, 0].bar(['평균', '중앙값'], [np.mean(a), np.median(a)], color='blue', alpha=0.7, label='변수 1')
+axes[0, 0].bar(['평균', '중앙값'], [np.mean(b), np.median(b)], color='green', alpha=0.7, label='변수 2')
+axes[0, 0].legend()
+axes[0, 0].set_title('기술 통계: 평균과 중앙값')
+
+# 상관 분석
+sns.heatmap(np.corrcoef(data.T), annot=True, ax=axes[0, 1])
+axes[0, 1].set_title('상관 분석')
+
+# 변수의 히스토그램
+axes[1, 0].hist([a, b], bins=15, color=['blue', 'green'], alpha=0.7, label=['변수 1', '변수 2'])
+axes[1, 0].legend()
+axes[1, 0].set_title('변수의 히스토그램')
+
+# 변수 1 대 변수 2의 산점도
+axes[1, 1].scatter(a, b, alpha=0.7)
+axes[1, 1].set_xlabel('변수 1')
+axes[1, 1].set_ylabel('변수 2')
+axes[1, 1].set_title('변수 1 대 변수 2의 산점도')
+
+# 레이아웃 조정 및 표시
+plt.tight_layout()
+plt.show()
 
